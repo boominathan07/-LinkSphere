d040284bee60c6cc558a5b2a1c4b769153687859
boominathan07 <boominathan7618@gmail.com>
2026-04-17 20:18:58 +0530

    Fix API base URL to production

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  withCredentials: true,
+    baseURL: "https://linksphere-d52y.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
